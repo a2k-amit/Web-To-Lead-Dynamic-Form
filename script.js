4c6e89c96f1c6962494904d071a0b03308f307c4
a2k-amit <amitkarlekar.sfdc@gmail.com>
2025-02-03 17:18:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Dynamic Web To Lead Form - Mini Project:');
+
+function beforesubmit() {
+    let outputdate = document.querySelector('.outputdate');
+    let inputdate = document.querySelector('.inputdate');
+
+    console.log('Input date value: ', inputdate.value); // type - string - convert >> date(en_IN)
+
+    let formattedDate = new Date (inputdate.value).toLocaleDateString('en-IN') // replace underscore with hyphen
+
+    // set the formatted for output date
+    outputdate.value = formattedDate;
+}
